65a93bf5c222c3f90927e0b55d2a0cc858b0cd0f
Ilya Lomatsky <ilomatsky@gmail.com>
2023-12-18 12:43:47 +0600

    Prepared the project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import React from 'react';
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
 import './App.css';
 
-const App = () => (
-  <>
-    App
-  </>
-);
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>TODO App</h1>
+      <TaskList/>
+      <TaskForm/>
+    </div>
+  );
+};
 
 export default App;
